f42f25afcff1924675954650da70013eb5deafb9
Tyler.Austgen <taustgen@gmail.com>
2017-03-09 10:40:43 -0800

    Beautified code

--- scripts/HomeController.js
+++ scripts/HomeController.js
@@ -1,11 +1,11 @@
 angular
-	.module('app')
-	.controller('HomeController', HomeController);
+    .module('app')
+    .controller('HomeController', HomeController);
 
-function HomeController($http){
-	var vm = this;
+function HomeController($http) {
+    var vm = this;
 
-$http.get('../topspots.json').then(function(response){
-		vm.in = response.data;
-});
+    $http.get('../topspots.json').then(function(response) {
+        vm.in = response.data;
+    });
 }
